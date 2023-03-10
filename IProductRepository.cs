4344b76f40c341ef307ed724a2cd558c5c3dc888
778116@cognizant.com <ammar.gangardiwala@cognizant.com>
2023-03-10 12:11:09 -0600

    deleted

--- IProductRepository.cs
+++
@@ -1,8 +0,0 @@
-public interface IProductRepository
-{
-    Task<List<Product>> GetAllAsync();
-    Task<Product> GetByIdAsync(int id);
-    Task AddAsync(Product product);
-    Task UpdateAsync(Product product);
-    Task DeleteAsync(int id);
-}
